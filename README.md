ce62a93c170da963ebaac67238a4f794808e3762
Miguel Marinhas <miguel.marinhas@gmail.com>
2011-11-11 00:30:29 +0000

    Prepare for heroku

---
+++ README.md
@@ -0,0 +1,11 @@
+# Ragebots
+
+## About
+Ragebots....
+
+## Run
+To run Ragebots you need:
+
+* Node.js (latest version prefered)
+* NPM (Node Package Manager)
+* Socket.io (latest version prefered)
---
+++ server.js
@@ -0,0 +1,10 @@
+var http = require('http');
+
+var port = process.env.PORT || 3000;
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World on heroku\n');
+}).listen(port);
+
+console.log('Server running at http://127.0.0.1:1337/');
