3b09a22a0a9eb5bda5ee712b00ab47748bc52f70
Miguel Marinhas <miguel.marinhas@gmail.com>
2011-11-11 20:44:20 +0000

    Port change

--- server.js
+++ server.js
@@ -69,7 +69,7 @@ app.configure('production', function(){
   //app.use(express.errorHandler());
 });
 
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 80;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
