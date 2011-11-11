9512905a172bb25f204ea2ee5d4d78284e62478e
Miguel Marinhas <miguel.marinhas@gmail.com>
2011-11-11 15:32:23 +0000

    Project base

--- server.js
+++ server.js
@@ -1,12 +1,117 @@
-var express = require('express');
+// Placeholder file for Node.js game server
+
+var util = require("util"),
+    express = require("express"),
+    io = require("socket.io"),
+    Player = require("./Player").Player;
 
 var app = express.createServer(express.logger());
+var socket = io.listen(app);
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
+socket.configure(function() {
+    socket.set("transports", ["websocket"]);
+    socket.set("log level", 2);
+});
+
+app.get('/index.htm', function (request, response) {
+    console.log("/index.htm");
+});
+
+app.configure(function(){
+    app.use(express.methodOverride());
+    app.use(express.bodyParser());
+    app.use(app.router);
+});
+
+app.configure('development', function(){
+    app.use(express.static(__dirname + '/public'));
+    //app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function(){
+  var oneYear = 31557600000;
+  app.use(express.static(__dirname + '/public', { maxAge: oneYear }));
+  //app.use(express.errorHandler());
 });
 
 var port = process.env.PORT || 3000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+var socket,
+    players;
+
+function init() {
+    players = [];
+
+    setEventHandlers();
+};
+
+var setEventHandlers = function() {
+    socket.sockets.on("connection", onSocketConnection);
+};
+
+function onSocketConnection(client) {
+    util.log("New player has connected: "+client.id);
+    client.on("disconnect", onClientDisconnect);
+    client.on("new player", onNewPlayer);
+    client.on("move player", onMovePlayer);
+};
+
+function onClientDisconnect() {
+    util.log("Player has disconnected: "+this.id);
+
+    var removePlayer = playerById(this.id);
+
+    if (!removePlayer) {
+        util.log("Player not found: "+this.id);
+        return;
+    };
+
+    players.splice(players.indexOf(removePlayer), 1);
+    this.broadcast.emit("remove player", {id: this.id});
+};
+
+function onNewPlayer(data) {
+    var newPlayer = new Player(data.x, data.y);
+    newPlayer.id = this.id;
+
+this.broadcast.emit("new player", {id: newPlayer.id, x: newPlayer.getX(), y: newPlayer.getY()});
+
+var i, existingPlayer;
+for (i = 0; i < players.length; i++) {
+    existingPlayer = players[i];
+    this.emit("new player", {id: existingPlayer.id, x: existingPlayer.getX(), y: existingPlayer.getY()});
+};
+
+players.push(newPlayer);
+
+};
+
+function onMovePlayer(data) {
+    var movePlayer = playerById(this.id);
+
+    if (!movePlayer) {
+        util.log("Player not found: "+this.id);
+        return;
+    };
+
+    movePlayer.setX(data.x);
+    movePlayer.setY(data.y);
+
+    this.broadcast.emit("move player", {id: movePlayer.id, x: movePlayer.getX(), y: movePlayer.getY()});
+};
+
+// Find player by ID
+function playerById(id) {
+    var i;
+    for (i = 0; i < players.length; i++) {
+        if (players[i].id == id)
+            return players[i];
+    };
+
+    return false;
+};
+
+init();
